d975fb00165a40c8f32b6f544b708f7c9c64e584
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-02 03:34:58 -0400

    feat: update social media

--- my-portfolio/src/components/Hero.tsx
+++ my-portfolio/src/components/Hero.tsx
@@ -1,17 +1,34 @@
-
-import { Space } from 'antd';
-import './hero.css'
+import { Space } from "antd";
+import "./hero.css";
 import { TiSocialFacebook } from "react-icons/ti";
+import { BiLogoTwitter } from "react-icons/bi";
+import { FaInstagram } from "react-icons/fa6";
+
 const Hero = () => {
   return (
     <div className="container">
-    <Space direction='vertical' size="large">
-      <h4 style={{ color: "white" , textAlign: "center"}}>WELCOME TO MY WEBSITE</h4>
-      <h1 className="type">I'am a Backend Developer</h1>
-      <ul><li style={{color: "white" , listStyle: "none" , fontSize: "24px"}}><TiSocialFacebook /></li></ul>
-    </Space>
-  </div>
-  )
-}
+      <Space direction="vertical" size="large">
+        <h4 style={{ color: "white", textAlign: "center" }}>
+          WELCOME TO MY WEBSITE
+        </h4>
+        <h1 className="type">I'am a Backend Developer</h1>
+        <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+          <li style={{ color: "white", listStyle: "none", fontSize: "24px" }}>
+            <TiSocialFacebook />
+          </li>
+          <li style={{ color: "white", listStyle: "none", fontSize: "24px" }}>
+            <BiLogoTwitter />
+          </li>
+          <li style={{ color: "white", listStyle: "none", fontSize: "24px" }}>
+            <FaInstagram />
+          </li>
+          <li style={{ color: "white", listStyle: "none", fontSize: "24px" }}>
+            <TiSocialFacebook />
+          </li>
+        </div>
+      </Space>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
