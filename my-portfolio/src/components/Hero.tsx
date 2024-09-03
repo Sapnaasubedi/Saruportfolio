8d1750b304054b97ead4c71d19ca749f65b90de1
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-03 01:09:17 -0400

    feat: update style

--- my-portfolio/src/components/Hero.tsx
+++ my-portfolio/src/components/Hero.tsx
@@ -7,7 +7,9 @@ import { FaInstagram } from "react-icons/fa6";
 const Hero = () => {
   return (
     <div className="container">
+      
       <Space direction="vertical" size="large">
+       
         <h4 style={{ color: "white", textAlign: "center" }}>
           WELCOME TO MY WEBSITE
         </h4>
@@ -27,10 +29,9 @@ const Hero = () => {
           </li>
         </div>
         <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
-          {" "}
           <Button size="large" style={{ borderRadius: "30px" }}>
             View Work
-          </Button>{" "}
+          </Button>
           <Button style={{ borderRadius: "30px" }} size="large">
             Hire Me
           </Button>
