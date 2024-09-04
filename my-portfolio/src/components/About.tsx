86f088caf18a97a6d5045fd7fd2d00141c9eb4ea
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-04 00:40:36 -0400

    feat: add about text content

--- my-portfolio/src/components/About.tsx
+++ my-portfolio/src/components/About.tsx
@@ -1,4 +1,4 @@
-import { Space, Typography, Image, Flex } from "antd";
+import { Space, Typography, Image, Flex, Button } from "antd";
 import "./hero.css";
 import AboutImage from "../assets/about.jpg";
 
@@ -6,13 +6,32 @@ const { Text } = Typography;
 const About = () => {
   return (
     <Flex justify="center">
-        <Space direction="vertical"><Text style={{ color: "white", fontSize: "28px", fontWeight: "bold" }}>
-        About Me
-      </Text>
-      <Space direction="horizontal">
-      <div className="about"> <Image width={200} src={AboutImage} /></div> 
-      </Space></Space>
-      
+      <Space direction="vertical">
+        <Text style={{ color: "white", fontSize: "28px", fontWeight: "bold" }}>
+          About Me
+        </Text>
+        <Space direction="horizontal">
+          <div className="about">
+            <Image width={300} src={AboutImage} />
+          </div>
+
+          <Space>
+            <Text style={{ color: "white", fontSize: "18px" }}>I am Saraswati Niroula</Text>
+            <Text style={{ color: "white", fontSize: "18px" }}>Backend Developer</Text>
+
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation laboris nisi ut
+              aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation laboris nisi ut aliquip ex ea commodo
+              consequat.
+            </Text>
+          </Space>
+        </Space>
+      </Space>
     </Flex>
   );
 };
