07bff95ffd0e4ba06e8c8c7710b300bcc02b09c3
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-04 00:35:40 -0400

    feat: add about image in ui

--- my-portfolio/src/components/About.tsx
+++ my-portfolio/src/components/About.tsx
@@ -1,12 +1,19 @@
-import { Typography } from "antd";
+import { Space, Typography, Image, Flex } from "antd";
 import "./hero.css";
+import AboutImage from "../assets/about.jpg";
 
 const { Text } = Typography;
 const About = () => {
   return (
-    <div className="about">
-      <Text style={{ color: "white" ,fontSize:"28px", fontWeight:"bold"}}>About Me </Text>
-    </div>
+    <Flex justify="center">
+        <Space direction="vertical"><Text style={{ color: "white", fontSize: "28px", fontWeight: "bold" }}>
+        About Me
+      </Text>
+      <Space direction="horizontal">
+      <div className="about"> <Image width={200} src={AboutImage} /></div> 
+      </Space></Space>
+      
+    </Flex>
   );
 };
 
