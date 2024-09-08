76e4bebd12ad16e71f02994ee3b0de1cbc83470d
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-08 03:05:40 -0400

    feat: add content

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Service from "./components/Service";
 
 function App() {
   return (
     <div>
       <Hero />
       <About/>
+      <Service />
     </div>
   );
 }
