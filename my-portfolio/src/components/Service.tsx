b17049179829e51e329f2373ab4cc5b4dfe379b1
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-08 03:01:14 -0400

    feat: style content

--- my-portfolio/src/components/Service.tsx
+++ my-portfolio/src/components/Service.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Space, Typography, Image, Flex, Button } from "antd";
+import "./hero.css";
+
+const { Text } = Typography;
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <Flex justify="center" style={{ margin: "50px" }}>
+    <Space direction="vertical" size="large" align="center">
+      <Text
+        style={{
+          color: "white",
+          fontSize: "30px",
+          fontWeight: "bold",
+          alignSelf: "center",
+        }}
+      >
+My Services        <div className="gradient-divider"></div>
+      </Text>
+
+     
+    </Space>
+  </Flex>
   )
 }
 
