7f193073cccf9f42871a7aa51d76df3d7da7b8b0
Sapana Subedi <sapanasubedi@takeo.ai>
2024-09-09 13:38:26 +0545

    feat: add swiper

--- my-portfolio/src/components/Service.tsx
+++ my-portfolio/src/components/Service.tsx
@@ -1,50 +1,11 @@
 import React from "react";
 import { Space, Typography,  Flex } from "antd";
 import "./hero.css";
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+
 const { Text } = Typography;
 
 const Service = () => {
-  const settings = {
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    centerMode: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dots: true,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-          infinite: true,
-          autoplay: true,
-          autoplaySpeed: 2000,
-        }
-      }
-    ]
-  };
+ 
   return (
     <Flex justify="center" style={{ margin: "50px" }}>
       <Space direction="vertical" size="large" align="center">
@@ -59,28 +20,7 @@ const Service = () => {
           My Services <div className="gradient-divider"></div>
         </Text>
       </Space>
-      <div className="wrapper">
-      <h2>Slick Carousel Example</h2>
-      <Slider {...settings} className="carousel">
-        {[1, 2, 3, 4, 5, 6].map((i) => (
-          <div key={i}>
-            <div className="card">
-              <div className="card-header">
-                <img src={`https://lorempixel.com/200/200/abstract/${i}`} alt={`Slide ${i}`} />
-              </div>
-              <div className="card-body">
-                <div className="card-content">
-                  <div className="card-title">This is the {i} slider</div>
-                  <div className="card-text">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </Slider>
-    </div>
+      
     </Flex>
   );
 };
---
+++ my-portfolio/src/components/swiper.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/no-array-index-key -- allow use of array index in keys*/
+"use client";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import ".hero.css";
+import { Col, Row } from "antd";
+import type { ReactNode } from "react";
+import React from "react";
+import { Navigation } from "swiper/modules";
+import type { SwiperProps } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { unnest } from "ramda";
+
+interface SwiperComponentProps extends SwiperProps {
+  children: ReactNode[];
+}
+
+export const SwiperComponent: React.FC<SwiperComponentProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return unnest(children).length > 3 ? (
+    <Swiper
+     
+      modules={[Navigation]}
+      navigation
+      slidesPerView={3}
+      spaceBetween={16}
+      {...props}
+    >
+      {unnest(children).map((slide, index) => (
+        <SwiperSlide key={index}>
+          {slide}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  ) : (
+    <Row gutter={16}>
+      {unnest(children).map((slide, index) => (
+        <Col key={index} span={8}>
+          {slide}
+        </Col>
+      ))}
+    </Row>
+  );
+};
